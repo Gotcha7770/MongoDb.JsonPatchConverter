d1cb0e6688356b34cdcccd6176df32eebd063e5c
Gotcha7770 <gbakanov@inbox.ru>
2023-02-24 17:09:37 +0300

    add type extensions

---
+++ MongoDb.JsonPatchConverter/TypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MongoDb.JsonPatchConverter
+{
+    public static class TypeExtensions
+    {
+        public static bool IsString(this Type type) => type == typeof(string);
+        public static bool IsDateTime(this Type type) => type == typeof(DateTime);
+        public static bool IsGuid(this Type type) => type == typeof(Guid);
+
+        public static bool SkipTypeProperties(this Type type)
+        {
+            return type.IsPrimitive 
+                   || type.IsEnum 
+                   || type.IsString() 
+                   || type.IsGuid() 
+                   || type.IsDateTime();
+        }
+    }
+}
